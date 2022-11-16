d46ad4501628ab571c32b20a6fa4d2f368ea3047
hebertcisco <hebertcisco@outlook.com>
2022-11-16 14:03:00 -0300

    feat: add addon header

---
+++ src/core/addon.h
@@ -0,0 +1,12 @@
+#ifndef NODE_CPP_NPM_PACKAGE_BOILERPLATE_ADDON
+#define NODE_CPP_NPM_PACKAGE_BOILERPLATE_ADDON
+
+#include <nan.h>
+#include "hello.h"
+
+#define LOG(msg) ;
+#define TRACEF(format, arg) ;
+
+#define TRACE(msg) LOG(msg);
+
+#endif
