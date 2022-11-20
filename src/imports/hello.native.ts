78338927f39503add73fff28ad9080a31f41499b
favelado-web <faveladoweb@gmail.com>
2022-11-19 22:34:29 -0300

    refactor: :construction: Adjusted the TypeScript implementation

--- src/imports/hello.native.ts
+++ src/imports/hello.native.ts
@@ -1,24 +1,5 @@
 import bindings from 'bindings';
 
-type HelloMethod = (name: string) => void;
+type Base = (string: string) => string;
 
-export interface IHelloNative {
-    prototype: {
-        hello: HelloMethod;
-        $hello: HelloMethod;
-    };
-    new(): IHelloNative;
-    hello: HelloMethod;
-}
-
-export const HelloNative: IHelloNative = bindings('addon.node').HelloNative;
-
-const helloNative = new HelloNative();
-
-helloNative.prototype.hello = function (name: string): void {
-    this.$hello(name);
-}
-
-helloNative.hello.prototype = helloNative.prototype;
-
-export default helloNative;
+export const helloNative: Base = bindings('hello.node').hello;
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-import helloNative from './imports/hello.native';
-
-export { HelloNative } from './imports/hello.native';
-export type { IHelloNative } from './imports/hello.native';
-
-helloNative.hello('world')
+export { helloNative } from './imports/hello.native';
