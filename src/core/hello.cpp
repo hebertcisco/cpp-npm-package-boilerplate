9f173ffbc05e132c8ca1e7387763bc746456c21d
hebertcisco <mrrabbitomg@gmail.com>
2023-10-21 16:31:06 -0400

    update hello.cpp

--- src/core/hello.cpp
+++ src/core/hello.cpp
@@ -1,13 +1,15 @@
 #include <napi.h>
 
-Napi::String hello(const Napi::CallbackInfo& info) {
+Napi::String Method(const Napi::CallbackInfo &info)
+{
     Napi::Env env = info.Env();
-    Napi::String name = info[0].As<Napi::String>();
-    return Napi::String::New(env, "Hello " + name.Utf8Value());
+    return Napi::String::New(env, "world");
 }
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports.Set(Napi::String::New(env, "hello"), Napi::Function::New(env, hello));
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+    exports.Set(Napi::String::New(env, "hello"),
+                Napi::Function::New(env, Method));
     return exports;
 }
 
